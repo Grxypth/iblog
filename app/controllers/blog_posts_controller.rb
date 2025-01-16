67db4d1cb5b6be64c0786dd3ebc1f9fd8b656889
İhsan Mirage <tezravasen@gmail.com>
2025-01-16 15:37:38 +0300

    feat: update action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,44 @@ class BlogPostsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
+
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, status: :not_found
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "/blog_posts/new" => "blog_posts#new", :as => :new_blog_post
   get "/blog_posts/:id" => "blog_posts#show", :as => :blog_post
+  patch "/blog_posts/:id", to: "blog_posts#update"
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  post "/blog_posts" => "blog_posts#create", :as => :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
