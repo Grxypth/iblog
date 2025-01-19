31d57d4150d309098043b509ba8446e2fa44e43f
İhsan Mirage <tezravasen@gmail.com>
2025-01-19 17:09:03 +0300

    finalize styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/blog_posts/new" => "blog_posts#new", :as => :new_blog_post
-  get "/blog_posts/:id" => "blog_posts#show", :as => :blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  post "/blog_posts" => "blog_posts#create", :as => :blog_posts
-
+  resources :blog_posts
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
---
+++ config/tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('preline/plugin'),
+    require('daisyui'),
+    // require('@tailwindcss/container-queries'),
+  ]
+}
